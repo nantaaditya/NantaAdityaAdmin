11f68a1f3ceed60a5195a10ad6cf209e70c76d47
pramuditya anantanur <pramuditza@gmail.com>
2018-08-12 08:04:37 +0700

    add UI path

---
+++ src/main/java/com/nantaaditya/admin/properties/UIPath.java
@@ -0,0 +1,17 @@
+package com.nantaaditya.admin.properties;
+// @formatter:off
+/**
+  * Author : Pramuditya Ananta Nur
+  * www.nantaaditya.com
+  * personal@nantaaditya.com
+  **/
+// @formatter:on
+
+public interface UIPath {
+
+  String LOGIN = "/login";
+  String DASHBOARD = "/dashboard";
+  String HOME_DASHBOARD = "/home-dashboard";
+  String BLOG_DASHBOARD = "/blog-dashboard";
+  String CONTACT_DASHBOARD = "/contact-dashboard";
+}
